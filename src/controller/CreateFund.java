6c1a0b6bc3dcc90ac62b1fc60c66ce2ca1fa24ed
Karan Dhingra <karand@andrew.cmu.edu>
2017-02-16 11:26:29 -0500

    correction in create fund

--- src/controller/CreateFund.java
+++ src/controller/CreateFund.java
@@ -5,25 +5,21 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.genericdao.DuplicateKeyException;
 import org.genericdao.RollbackException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import databeans.Customer;
 import databeans.Fund;
 import formbeans.CreateFundForm;
-import model.CustomerDAO;
 import model.FundDAO;
 import model.Model;
 
 public class CreateFund extends Action{
 	private FundDAO fundDAO;
-	private Customer admin;
-	private CustomerDAO customerDAO;
 	
 	public CreateFund(Model model) {
-		customerDAO = model.getCustomerDAO();
 		fundDAO = model.getFundDAO();
 	}
 	@Override
@@ -33,9 +29,18 @@ public class CreateFund extends Action{
 
 	public String perform (HttpServletRequest request) {
 		JsonObject obj = new JsonObject();
-		Fund fund = new Fund();
 		BufferedReader br;
 		String line;
+		
+		if(request.getSession().getAttribute("employee")== null && request.getSession().getAttribute("customer") == null){
+			obj.addProperty("message", "You are not currently logged in");
+			return obj.toString();
+		}
+		
+		if(request.getSession().getAttribute("employee") == null) {
+			obj.addProperty("message", "You must be an employee to perform this action");
+			return obj.toString();
+		}
 		
 		try {
 			br = request.getReader();
@@ -44,22 +49,18 @@ public class CreateFund extends Action{
 			CreateFundForm form = gson.fromJson(line, CreateFundForm.class);
 			if (form.hasErrors()) {
 				obj.addProperty("message", "The input you provided is not valid");
-			}
-			if(request.getSession().getAttribute("employee")== null && request.getSession().getAttribute("customer") == null){
-				obj.addProperty("message", "You are not currently logged in");
-				return obj.toString();
-			}
-			if(request.getSession().getAttribute("employee") == null) {
-				obj.addProperty("message", "You must be an employee to perform this action");
-				return obj.toString();
 			} else {
+				Fund fund = new Fund();
 	            fund.setName(form.getName().toUpperCase());
 	            fund.setSymbol(form.getSymbol().toUpperCase());
+	            fund.setInitial_value(Double.parseDouble(form.getInitial_value()));
 	            fundDAO.create(fund);
 				obj.addProperty("message", "The fund was successfully created");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch(DuplicateKeyException e) {
+			obj.addProperty("message", "The input you provided is not valid");
 		} catch (RollbackException e) {
 			e.printStackTrace();
 		}
